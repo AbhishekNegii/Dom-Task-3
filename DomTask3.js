bcad0c4936b59909eadc12b6683c604276864a3b
Abhishek <abhibaba007@gmail.com>
2022-07-06 18:48:28 +0530

    New  Dom Task

---
+++ DomTask3.js
@@ -0,0 +1,20 @@
+//console.dir(document);
+//console.log(document.all)
+//console.log(document.domain)
+//console.log(document.URL)
+//console.log(document.title)
+//document.title=123
+//console.log(document.body)
+//console.log(document.head)
+//console.log(document.links)
+//var headerTitle= document.getElementById='header-title'
+//header.style.borderBottom = 'solid 6px #000'
+//var items=document.getElementsByClassName('list-group-item')
+//console.log(items)
+//console.log(items[1])
+//items[1].textContent='Hello'
+var title=document.getElementsByClassName('title')
+console.log(title)
+console.log(title[0])
+title[0].style.fontWeight='Bold'
+title[0].style.color='Yellow'
