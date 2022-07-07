d84e90c88d6c4ecb9d26cb0d4b6fb59b84f56fcb
Abhishek <abhibaba007@gmail.com>
2022-07-07 11:58:33 +0530

    querySelector and querySelectorAll Added

--- DomTask5.js
+++ DomTask5.js
@@ -8,13 +8,33 @@
 //     items[i].style.fontWeight='bold'
 
 //Get Element By Tag Name
-var li= document.getElementsByTagName('li')
-console.log(li)
-console.log(li[1])
-li[1].textContent='Hello'
-li[2].style.backgroundColor='green'
-for(var i=0;i<li.length;i++)
-    li[i].style.fontWeight='bold'
-li[4].style.backgroundColor='yellow'
-li[4].style.fontWeight='bold'
-    
+// var li= document.getElementsByTagName('li')
+// console.log(li)
+// console.log(li[1])
+// li[1].textContent='Hello'
+// li[2].style.backgroundColor='green'
+// for(var i=0;i<li.length;i++)
+//     li[i].style.fontWeight='bold'
+// li[4].style.backgroundColor='yellow'
+// li[4].style.fontWeight='bold'
+
+//querySelector
+
+var secondItem=document.querySelector('.list-group-item:nth-child(2)')
+secondItem.style.backgroundColor='blue'
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)')
+thirdItem.style.visibility='hidden'
+
+//querySelectorAll
+
+var titles=document.querySelectorAll('.title')
+console.log(titles)
+titles[1].style.color='green'
+
+var odd=document.querySelectorAll('li:nth-child(odd)')
+for(var i=0;i<odd.length;i++)
+odd[i].style.backgroundColor='green'
+
+// querySelector - querySelector() method can only be use to access a single element
+
+// querySelectorAll - querySelectorAll() method can be used to access all element
