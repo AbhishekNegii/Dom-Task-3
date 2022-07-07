60334a95fef8460672537d660d96aac538b7478c
Abhishek <abhibaba007@gmail.com>
2022-07-07 10:54:43 +0530

    Dom Task 5

---
+++ DomTask5.js
@@ -0,0 +1,20 @@
+//Get Element By Class Name
+// var items= document.getElementsByClassName('list-group-item')
+// console.log(items)
+// console.log(items[1])
+// items[1].textContent='Hello'
+// items[2].style.backgroundColor='green'
+// for(var i=0;i<items.length;i++)
+//     items[i].style.fontWeight='bold'
+
+//Get Element By Tag Name
+var li= document.getElementsByTagName('li')
+console.log(li)
+console.log(li[1])
+li[1].textContent='Hello'
+li[2].style.backgroundColor='green'
+for(var i=0;i<li.length;i++)
+    li[i].style.fontWeight='bold'
+li[4].style.backgroundColor='yellow'
+li[4].style.fontWeight='bold'
+    
